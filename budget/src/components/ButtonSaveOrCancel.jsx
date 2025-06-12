0756abe4f8f37bc488219290f4e562f3e6926026
Jose Arias <josee.ariasmelo@gmail.com>
2025-06-12 17:03:46 -0300

    Refactor: Actualiza función addEntry para incluir tipo de gasto
    Modifica la función `handleClick` para enviar el tipo de gasto (ingreso o egreso) junto con la descripción y el valor.
    Esto permite distinguir entre entradas de ingresos y egresos al momento de agregar un registro.

--- budget/src/components/ButtonSaveOrCancel.jsx
+++ budget/src/components/ButtonSaveOrCancel.jsx
@@ -1,21 +1,16 @@
 import React from 'react'
 
-const ButtonSaveOrCancel = ({ addEntry, description, value }) => {
+const ButtonSaveOrCancel = ({ addEntry, description, value, isExpense }) => {
     
     const handleClick = (e) => {
         e.preventDefault(); // Previene el submit del formulario
-        addEntry(description, Number(value));
+        console.log(isExpense);
+        
+        addEntry(description, Number(value), isExpense);
     };
 
     return (
-        <>
-            {/* <input
-                className="form-check-input"
-                type="checkbox"
-            />
-            <label className="form-check-label" htmlFor="isExpense">
-                Is Expense
-            </label> */}
+        <>            
             <button onClick={handleClick}> ok </button>
             <button> cancel </button>
         </>
