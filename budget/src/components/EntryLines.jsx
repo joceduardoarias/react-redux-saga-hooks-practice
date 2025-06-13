1da58f9753cc777175d80d6965c81cfd48eed2d2
Jose Arias <josee.ariasmelo@gmail.com>
2025-06-13 11:58:44 -0300

    Pasa función para abrir el modal
    Pasa la función setIsOpen al componente EntryLine para que pueda abrir el modal de edición.

--- budget/src/components/EntryLines.jsx
+++ budget/src/components/EntryLines.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import EntryLine from "./EntryLine"
 
-const EntryLines = ({ entries, deleteEntry }) => {
+const EntryLines = ({ entries, deleteEntry, setIsOpen }) => {
     
   return (
     <>
     {
         entries.map(entry =>(
-        <EntryLine key={ entry.id } { ...entry } deleteEntry={ deleteEntry }/>    
+        <EntryLine key={ entry.id } { ...entry } deleteEntry={ deleteEntry } setIsOpen={setIsOpen}/>    
         ))
       }
     </>
