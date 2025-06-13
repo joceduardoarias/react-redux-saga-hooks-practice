f33d075227f07c7df3e44e486020dddb5b76ba80
Jose Arias <josee.ariasmelo@gmail.com>
2025-06-13 12:24:48 -0300

    Pasa editEntry a EntryLine
    Pasa la función editEntry al componente EntryLine para habilitar la funcionalidad de edición directamente desde cada línea de entrada. Esto mejora la organización del código y facilita la interacción del usuario con las entradas individuales.

--- budget/src/components/EntryLines.jsx
+++ budget/src/components/EntryLines.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import EntryLine from "./EntryLine"
 
-const EntryLines = ({ entries, deleteEntry, setIsOpen }) => {
+const EntryLines = ({ entries, deleteEntry, editEntry }) => {
     
   return (
     <>
     {
         entries.map(entry =>(
-        <EntryLine key={ entry.id } { ...entry } deleteEntry={ deleteEntry } setIsOpen={setIsOpen}/>    
+        <EntryLine key={ entry.id } { ...entry } deleteEntry={ deleteEntry } editEntry={editEntry}/>    
         ))
       }
     </>
