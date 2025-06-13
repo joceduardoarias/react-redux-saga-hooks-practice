c25b26e74f8ad888cc039f0fd1a1a07adf43540c
Jose Arias <josee.ariasmelo@gmail.com>
2025-06-13 11:58:20 -0300

    Refactor: Simplifica el formulario NewEntryForm
    Elimina el estado local del componente NewEntryForm, moviendo la gestión del estado al componente padre para mejorar la reutilización y el mantenimiento.

--- budget/src/components/NewEntryForm.jsx
+++ budget/src/components/NewEntryForm.jsx
@@ -2,11 +2,8 @@ import React, { useState } from 'react'
 import ButtonSaveOrCancel from "../components/ButtonSaveOrCancel"
 import EntryForm from './EntryForm'
 
-const NewEntryForm = ({ addEntry }) => {
-    const [description, setDescription] = useState('')
-    const [value, setValue] = useState(0)
-    const [isExpense, setIsExpense] = useState(false)
-
+const NewEntryForm = ({ addEntry, description, setDescription, value, setValue, isExpense, setIsExpense }) => {
+    
     return (
         <form className="bg-light rounded shadow p-4 mb-4" style={{ maxWidth: 600, margin: "0 auto" }}>
             <EntryForm
