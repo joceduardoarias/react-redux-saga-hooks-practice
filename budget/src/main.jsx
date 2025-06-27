982c86e3753c58dd84daacd8932c49eee73ebed8
Jose Arias <josee.ariasmelo@gmail.com>
2025-06-27 10:20:33 -0300

    Integra Redux para manejo de estado global
    Configura Redux para centralizar el manejo de estado de la aplicación.
    Envuelve el componente App con el Provider de Redux.
    Esto permite acceder al store desde cualquier componente.

--- budget/src/main.jsx
+++ budget/src/main.jsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { Provider } from 'react-redux';
+import createAppStore from './stores/configureStore.jsx';
+
+const store = createAppStore()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
