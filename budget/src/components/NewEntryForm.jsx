bf9e7cc500bb431e96ba247ac49ea15218f790d4
Jose Arias <josee.ariasmelo@gmail.com>
2025-06-27 12:42:33 -0300

    Feat: Implementar adición de entradas con Redux
    Implementa la funcionalidad para añadir nuevas entradas de presupuesto utilizando Redux.
    
    Este cambio permite gestionar el estado de las entradas de presupuesto de forma centralizada, facilitando la gestión de datos y la actualización de la interfaz de usuario.

--- budget/src/components/NewEntryForm.jsx
+++ budget/src/components/NewEntryForm.jsx
@@ -1,9 +1,25 @@
 import React, { useState } from 'react'
 import ButtonSaveOrCancel from "../components/ButtonSaveOrCancel"
 import EntryForm from './EntryForm'
+import { useDispatch } from 'react-redux'
+import { addEntryRedux } from "../actions/entries.actions";
 
-const NewEntryForm = ({ addEntry, description, setDescription, value, setValue, isExpense, setIsExpense }) => {
+const NewEntryForm = () => {
+    const [description, setDescription] = useState('')
+    const [value, setValue] = useState(0)
+    const [isExpense, setIsExpense] = useState(false)
+    const dispatch = useDispatch()
     
+    const addEntry = () => {
+        dispatch(addEntryRedux({
+            description,
+            value,
+            isExpense
+        }))
+        setDescription('')
+        setValue(0)
+        setIsExpense(false)
+    }
     return (
         <form className="bg-light rounded shadow p-4 mb-4" style={{ maxWidth: 600, margin: "0 auto" }}>
             <EntryForm
