c0bacf3e67b3e897f227f89f60f5253f89dfa30e
Jose Arias <josee.ariasmelo@gmail.com>
2025-06-27 10:49:27 -0300

    Refactor: Simplifica payload en removeEntryRedux
    Reduce la complejidad del payload en la acción removeEntryRedux, enviando directamente el ID en lugar de un objeto. Esto facilita el manejo de la información y mejora la legibilidad del código.

--- budget/src/actions/entries.actions.jsx
+++ budget/src/actions/entries.actions.jsx
@@ -2,6 +2,6 @@ export const addEntryRedux = (payload) => {
     return { type: 'ADD_ENTRY', payload }
 }
 
-export const removeEntryRedux = (id) => {
-    return { type: 'REMOVE_ENTRY', payload: {id} }
+export const removeEntryRedux = (id) => {        
+    return { type: 'REMOVE_ENTRY', payload: id }
 }
