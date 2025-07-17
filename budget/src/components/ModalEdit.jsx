fa2e7d4807aa412d2def1c01404438f5adec336f
Jose Arias <josee.ariasmelo@gmail.com>
2025-07-17 18:00:44 -0300

    Feat: Implementar modal de edición de entrada
    Implementa el modal de edición de entrada para permitir a los usuarios modificar los detalles de una entrada existente.
    
    Este modal utiliza el componente `EntryForm` para mostrar los campos editables y permite guardar o cancelar los cambios.

--- budget/src/components/ModalEdit.jsx
+++ budget/src/components/ModalEdit.jsx
@@ -1,29 +1,35 @@
-import React from 'react'
 import EntryForm from './EntryForm';
+import { useDispatch } from 'react-redux'
+import { closeEditModal } from "../actions/modals.actions"
+import { useEntryDetails } from "../hooks/useEntryDetails";
 
-const ModalEdit = ({ isOpen, setIsOpen, description, setDescription, value, setValue, isExpense, setIsExpense }) => {
+const ModalEdit = ({ isOpen, description, value, isExpense }) => {
+        
+    const entryUpadte = useEntryDetails(description, value, isExpense)
+    const dispatch = useDispatch();
     if (!isOpen) return null;
+    
     return (
         <div className="modal show fade d-block" tabIndex="-1" style={{ background: "rgba(0,0,0,0.5)" }}>
             <div className="modal-dialog">
                 <div className="modal-content">
                     <div className="modal-header">
                         <h5 className="modal-title">Edit entry</h5>
-                        <button type="button" className="btn-close" onClick={() => setIsOpen(false)}></button>                        
+                        <button type="button" className="btn-close" onClick={() => dispatch(closeEditModal())}></button>
                     </div>
                     <div className="modal-body">
-                        <EntryForm description={description}
-                            value={value}
-                            isExpense={isExpense}
-                            setDescription={setDescription}
-                            setValue={setValue}
-                            setIsExpense={setIsExpense} />
+                        <EntryForm description={entryUpadte.description}
+                            value={entryUpadte.value}
+                            isExpense={entryUpadte.isExpense}
+                            setDescription={entryUpadte.setDescription}
+                            setValue={entryUpadte.setValue}
+                            setIsExpense={entryUpadte.setIsExpense} />
                     </div>
                     <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" onClick={() => setIsOpen(false)}>
+                        <button type="button" className="btn btn-secondary" onClick={() => dispatch(closeEditModal())}>
                             Cerrar
-                        </button>                      
-                        <button type="button" className="btn btn-primary" onClick={() => setIsOpen(false)}>
+                        </button>
+                        <button type="button" className="btn btn-primary" onClick={() => dispatch(closeEditModal())}>
                             Guardar cambios
                         </button>
                     </div>
