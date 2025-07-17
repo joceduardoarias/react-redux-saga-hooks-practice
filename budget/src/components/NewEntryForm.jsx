74f1b16eb7522cfd4cfb523690d95a8e8ce9a0bc
Jose Arias <josee.ariasmelo@gmail.com>
2025-07-17 18:01:08 -0300

    Refactoriza: Simplifica formulario de nueva entrada
    Reemplaza el uso de Redux para gestionar el estado del formulario de nueva entrada con un hook personalizado, mejorando la mantenibilidad y legibilidad del código.

--- budget/src/components/NewEntryForm.jsx
+++ budget/src/components/NewEntryForm.jsx
@@ -1,38 +1,23 @@
-import React, { useState } from 'react'
 import ButtonSaveOrCancel from "../components/ButtonSaveOrCancel"
 import EntryForm from './EntryForm'
-import { useDispatch } from 'react-redux'
-import { addEntryRedux } from "../actions/entries.actions";
+import { useEntryDetails } from "../hooks/useEntryDetails";
 
 const NewEntryForm = () => {
-    const [description, setDescription] = useState('')
-    const [value, setValue] = useState(0)
-    const [isExpense, setIsExpense] = useState(false)
-    const dispatch = useDispatch()
+    const newEntry = useEntryDetails()    
     
-    const addEntry = () => {
-        dispatch(addEntryRedux({
-            description,
-            value,
-            isExpense
-        }))
-        setDescription('')
-        setValue(0)
-        setIsExpense(false)
-    }
     return (
         <form className="bg-light rounded shadow p-4 mb-4" style={{ maxWidth: 600, margin: "0 auto" }}>
             <EntryForm
-                description={description}
-                value={value}
-                isExpense={isExpense}
-                setDescription={setDescription}
-                setValue={setValue}
-                setIsExpense={setIsExpense}
+                description={newEntry.description}
+                value={newEntry.value}
+                isExpense={newEntry.isExpense}
+                setDescription={newEntry.setDescription}
+                setValue={newEntry.setValue}
+                setIsExpense={newEntry.setIsExpense}
             />
             <div className="d-flex gap-2 justify-content-end mt-3">
                 <ButtonSaveOrCancel
-                    addEntry={addEntry}                    
+                    addEntry={newEntry.addEntry}                    
                 />
             </div>
         </form>
