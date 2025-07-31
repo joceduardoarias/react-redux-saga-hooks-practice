376e60b943ae85c6918905148c5f0f9ca11a45c2
Jose Arias <josee.ariasmelo@gmail.com>
2025-07-31 11:31:33 -0300

    Simplifica cierre del modal de edición.
    Elimina el payload innecesario al cerrar el modal de edición, ya que no se utiliza. Esto simplifica la acción y la hace más eficiente.

--- budget/src/actions/modals.actions.jsx
+++ budget/src/actions/modals.actions.jsx
@@ -2,6 +2,6 @@ export const openEditModal = (id) => {
     return { type: 'OPEN_EDIT_MODAL', payload: {id} }
 }
 
-export const closeEditModal = (payload) => {
-    return { type: 'CLOSE_EDIT_MODAL', payload }
+export const closeEditModal = () => {
+    return { type: 'CLOSE_EDIT_MODAL' }
 }
