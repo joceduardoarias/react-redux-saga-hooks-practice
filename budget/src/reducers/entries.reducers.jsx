6331aa9586ff882683323991601e118af93758ec
Jose Arias <josee.ariasmelo@gmail.com>
2025-08-08 17:10:52 -0300

    Removes initial entries
    Removes the initial hardcoded entries from the entries reducer. This prepares the application for fetching entries from an external source or allowing users to create their own entries.

--- budget/src/reducers/entries.reducers.jsx
+++ budget/src/reducers/entries.reducers.jsx
@@ -24,28 +24,5 @@ const reducer = (state = initialEntries, action) => {
 export default reducer
 
 var initialEntries = [
-  {
-    id: 1,
-    description: "Work income redux",
-    value: 1000.00,
-    isExpense: true
-  },
-  {
-    id: 2,
-    description: "Waater bill redux",
-    value: 20.00,
-    isExpense: true
-  },
-  {
-    id: 3,
-    description: "Rent redux",
-    value: 300.00,
-    isExpense: false
-  },
-  {
-    id: 4,
-    description: "Power bill redux",
-    value: 50.00,
-    isExpense: true
-  },
+ 
 ]
