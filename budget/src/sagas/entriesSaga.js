23971867ae4c667a7767dce1bea588c3b32d027e
Jose Arias <josee.ariasmelo@gmail.com>
2025-08-08 17:10:30 -0300

    Feat: Configurar saga para obtener entradas
    Inicializa la saga para gestionar la obtención de entradas.
    
    Configura el watcher que escucha la acción GET_ENTRIES y
    temporalmente muestra un mensaje en consola.

---
+++ budget/src/sagas/entriesSaga.js
@@ -0,0 +1,8 @@
+import { take } from "redux-saga/effects";
+import entriesTypes from "../actions/entries.actions";
+
+export function* getAllEntries() {
+    yield take(entriesTypes.GET_ENTRIES)   
+    console.log('I need to get the entries now');
+     
+}
