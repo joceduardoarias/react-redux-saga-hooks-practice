6b81bd4509b271ef2a8cf945e3312e7b112b1505
Jose Arias <josee.ariasmelo@gmail.com>
2025-08-08 16:25:31 -0300

    Refactor: Simplificar gestión de tipos de acción
    Centraliza la definición de tipos de acción en un solo archivo
    para mejorar la mantenibilidad y evitar errores de tipeo.
    
    Facilita la reutilización de los tipos de acción en diferentes
    partes de la aplicación (actions y reducers).

--- budget/src/actions/entries.actions.jsx
+++ budget/src/actions/entries.actions.jsx
@@ -1,3 +1,11 @@
+const types = {
+    ADD_ENTRY: 'ADD_ENTRY',
+    REMOVE_ENTRY: 'REMOVE_ENTRY',
+    UPDATE_ENTRY: 'UPDATE_ENTRY'
+}
+
+export default types
+
 export const addEntryRedux = (payload) => {
     return { type: 'ADD_ENTRY', payload }
 }
@@ -7,5 +15,5 @@ export const removeEntryRedux = (id) => {
 }
 
 export const updateEntryRedux = (id, entry) => {    
-    return { type: "UPDATE_ENTRY", payload: { id, entry } }
+    return { type: 'UPDATE_ENTRY', payload: { id, entry } }
 }
--- budget/src/reducers/entries.reducers.jsx
+++ budget/src/reducers/entries.reducers.jsx
@@ -1,15 +1,17 @@
+import types from "../actions/entries.actions";
+
+
 const reducer = (state = initialEntries, action) => {
   switch (action.type) {
-    case "ADD_ENTRY":
+    case types.ADD_ENTRY:
       const nextId = state.length + 1
       return [...state, { ...action.payload, id: nextId }];
-    case "REMOVE_ENTRY":
+    case types.REMOVE_ENTRY:
       return state.filter(entry => {
         return entry.id != action.payload
       })
-    case "UPDATE_ENTRY":
-      console.log(action);
-      
+    case types.UPDATE_ENTRY:
+            
       const newEntries = [...state]
       const index = newEntries.findIndex(entry => entry.id === action.payload.id)
       newEntries[index] = { ...action.payload.entry }
