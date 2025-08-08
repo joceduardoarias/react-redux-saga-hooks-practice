58d3ba1511080cb323b11a0dc5ee78120a05b328
Jose Arias <josee.ariasmelo@gmail.com>
2025-08-08 17:10:41 -0300

    Define acciones y tipos de entrada Redux
    Define los tipos de acción para la gestión de entradas
    (GET, ADD, REMOVE, UPDATE) y sus funciones creadoras
    correspondientes para interactuar con el store de Redux.

--- budget/src/actions/entries.actions.jsx
+++ budget/src/actions/entries.actions.jsx
@@ -1,19 +1,24 @@
-const types = {
+const entriesTypes = {
+    GET_ENTRIES: 'GET_ENTRIES',
     ADD_ENTRY: 'ADD_ENTRY',
     REMOVE_ENTRY: 'REMOVE_ENTRY',
     UPDATE_ENTRY: 'UPDATE_ENTRY'
 }
 
-export default types
+export default entriesTypes
 
 export const addEntryRedux = (payload) => {
-    return { type: 'ADD_ENTRY', payload }
+    return { type: entriesTypes.ADD_ENTRY, payload }
 }
 
 export const removeEntryRedux = (id) => {            
-    return { type: 'REMOVE_ENTRY', payload: id }
+    return { type: entriesTypes.REMOVE_ENTRY, payload: id }
 }
 
 export const updateEntryRedux = (id, entry) => {    
-    return { type: 'UPDATE_ENTRY', payload: { id, entry } }
+    return { type: entriesTypes.UPDATE_ENTRY, payload: { id, entry } }
+}
+
+export const getAllEntriesRedux = () => {    
+    return { type: entriesTypes.GET_ENTRIES }
 }
