1b371b3815453e186836744c5e9bec0072fc9e0c
Jose Arias <josee.ariasmelo@gmail.com>
2025-08-08 16:26:05 -0300

    Refactor: Simplifica el manejo de modales
    Unifica los tipos de acciones de modales en un objeto constante.
    
    Esto mejora la mantenibilidad y evita errores tipográficos al referenciar las acciones en el reducer.

--- budget/src/actions/modals.actions.jsx
+++ budget/src/actions/modals.actions.jsx
@@ -1,3 +1,9 @@
+const modalTypes = {
+    OPEN_EDIT_MODAL: 'OPEN_EDIT_MODAL',
+    CLOSE_EDIT_MODAL: 'CLOSE_EDIT_MODAL'
+}
+export default modalTypes 
+
 export const openEditModal = (id) => {    
     return { type: 'OPEN_EDIT_MODAL', payload: {id} }
 }
