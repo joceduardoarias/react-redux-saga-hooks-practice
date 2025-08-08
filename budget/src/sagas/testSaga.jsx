19582de8a616dadcd58edaac27b7b654e15b8269
Jose Arias <josee.ariasmelo@gmail.com>
2025-08-08 15:55:57 -0300

    Refactoriza: Mejora la lógica de la saga de prueba
    Refactoriza la saga de prueba para incluir efectos de Redux Saga como 'call' y 'delay',
    permitiendo un mejor control sobre el flujo de ejecución y facilitando las pruebas.
    Además, elimina código redundante y simplifica la estructura.

--- budget/src/sagas/testSaga.jsx
+++ budget/src/sagas/testSaga.jsx
@@ -1,19 +1,27 @@
-import { take } from "redux-saga/effects";
+import { delay, put, take, call } from "redux-saga/effects";
 
 export function* testSaga() {
    while (true) {
       console.log("Starting saga");
-      yield take('TEST_MESSAGE')
-      console.log("Finish saga function");
+      const state = yield take('TEST_MESSAGE')
+      const a = yield call(double, 2)
+      console.log(a);
+      const b = yield double(3)
+      console.log(b);
+      
+      console.log("Finish saga function", state);
 
    }
 
 }
 
-export function* count() {
-   yield 1
-   yield 2
-   yield 3
-   yield 4
-   yield 5      
+function double(number) {
+   return number * 2
+}
+
+export function* dispatchTest() {
+   while (true) {
+      yield delay(1000)
+      yield put({ type: 'TEST_MESSAGE', payload: 1000 })           
+   }
 }
