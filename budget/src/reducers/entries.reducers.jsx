0851facdf51975b6118e70f693175f634bebdd1b
Jose Arias <josee.ariasmelo@gmail.com>
2025-08-12 17:05:42 -0300

    Feat: Implementar la gestión de entradas.
    Implementa la lógica para poblar, añadir, eliminar y actualizar las entradas del presupuesto.
    Esto permite una manipulación completa de los datos del presupuesto en la aplicación.

--- budget/src/reducers/entries.reducers.jsx
+++ budget/src/reducers/entries.reducers.jsx
@@ -2,20 +2,30 @@ import types from "../actions/entries.actions";
 
 
 const reducer = (state = initialEntries, action) => {
+
   switch (action.type) {
+    case types.POPULATE_ENTRIES:
+                  
+      return action.payload
+
     case types.ADD_ENTRY:
+
       const nextId = state.length + 1
       return [...state, { ...action.payload, id: nextId }];
+
     case types.REMOVE_ENTRY:
+
       return state.filter(entry => {
         return entry.id != action.payload
       })
+
     case types.UPDATE_ENTRY:
-            
+
       const newEntries = [...state]
       const index = newEntries.findIndex(entry => entry.id === action.payload.id)
       newEntries[index] = { ...action.payload.entry }
       return newEntries
+
     default:
       return state
   }
@@ -24,5 +34,5 @@ const reducer = (state = initialEntries, action) => {
 export default reducer
 
 var initialEntries = [
- 
+
 ]
