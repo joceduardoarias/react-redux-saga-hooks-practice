1771287de596c8b12a22079ca8e8e6e31d81682d
Jose Arias <josee.ariasmelo@gmail.com>
2025-09-09 12:26:05 -0300

    Feat: Implementar saga para obtener entradas
    Implementa una saga para obtener todas las entradas desde la API y poblar el estado de la aplicación.
    También incluye la implementación inicial para obtener detalles de las entradas.

--- budget/src/sagas/entriesSaga.js
+++ budget/src/sagas/entriesSaga.js
@@ -1,8 +1,28 @@
-import { take } from "redux-saga/effects";
+import { take, call, put, fork } from "redux-saga/effects";
 import entriesTypes from "../actions/entries.actions";
+import Axios from 'axios'
+
 
 export function* getAllEntries() {
-    yield take(entriesTypes.GET_ENTRIES)   
-    console.log('I need to get the entries now');
-     
+
+    const result = yield call(Axios, 'http://localhost:5000/entries')
+    console.log(result.data);
+
+    yield put({ type: entriesTypes.POPULATE_ENTRIES, payload: result.data })
+}
+
+export function* getAllEntriesDetails() {
+    const { data } = yield call(Axios, 'http://localhost:5000/values')
+
+
+    for (let index = 0; index < data.length; index++) {
+        const entry = data[index];
+        yield fork(getEntriesDetails, entry.id)
+    }
+
+}
+
+export function* getEntriesDetails(id) {
+    console.log("got the id ", id);
+
 }
