19f8dad5f00efa73a706a38e574c59a944967bbc
Jose Arias Melo <josee.ariasmelo@gmail.com>
2025-09-23 15:58:39 -0300

    Adds saga for adding entries to the database
    Implements a saga to handle adding new entries to the database.
    The saga listens for the ADD_ENTRY action, calls the API to save the entry,
    and then dispatches an action with the result.
    The `getAllEntriesDetails` saga is temporarily disabled to ensure data consistency.

--- budget/src/sagas/entriesSagaAdd.js
+++ budget/src/sagas/entriesSagaAdd.js
@@ -2,22 +2,26 @@ import { call, put, take, takeLatest } from "redux-saga/effects";
 import entriesTypes from "../actions/entries.actions";
 import axios from "axios";
 
-export function* addEntrySaga() {    
-    yield takeLatest(entriesTypes.ADD_ENTRY, addEntryToDb)    
+export function* addEntrySaga() {
+    yield takeLatest(entriesTypes.ADD_ENTRY, addEntryToDb)
 }
 
-function* addEntryToDb({payload}) {
-    console.log("addEntryToDb: ",payload);
-    
-  const savedEntry = yield call(addEntry, payload);  
-  yield put({ type: entriesTypes.ADD_ENTRY_RESULT, payload: savedEntry });
+function* addEntryToDb({ payload }) {
+    console.log("addEntryToDb: ", payload);
+
+    const savedEntry = yield call(addEntry, payload);
+    console.log("savedEntry: ", savedEntry);
+    yield put({ type: entriesTypes.ADD_ENTRY_RESULT, payload: savedEntry }); // Dispatch action to update the store
 }
 
-function addEntry(newEntry) {
-    axios.post(`http://localhost:5000/entries`, newEntry)
-        .then(response => response.data)
-        .catch(error => {
-            // Manejar el error
-            console.error('Hubo un error al crear el post:', error);
-        });
+async function addEntry(newEntry) {
+    console.log("addEntry: ", newEntry);
+
+    try {
+        const response = await axios.post(`http://localhost:5000/entries`, newEntry);
+        return response.data;
+    } catch (error) {
+        // Manejar el error
+        console.error('Hubo un error al crear el post:', error);
+    };
 }
--- budget/src/sagas/index.js
+++ budget/src/sagas/index.js
@@ -2,14 +2,16 @@ import { dispatchTest, testSagaFork } from './testSaga'
 import { getAllEntries, getAllEntriesDetails } from './entriesSaga'
 import { all } from 'redux-saga/effects'
 import { deleteEntrySaga } from './entriesSagaDeletion'
+import { addEntrySaga } from './entriesSagaAdd'
 
 export function* rootSaga() {
     // Object.values(getAllEntries).forEach(sagaMiddleWare.run.bind(sagaMiddleWare))
     yield all([
         getAllEntries(),
         // testSagaFork(),        
+        addEntrySaga(),
         deleteEntrySaga(),
         dispatchTest(),
-        getAllEntriesDetails()
+        // getAllEntriesDetails() -> Se comenta porque los valores de entries y values deben tener datos sincronizados
     ])
 }
